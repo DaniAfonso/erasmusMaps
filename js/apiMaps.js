cfb41859edc2ac5de104a2e1b148ca2dfbfc77c4
Daniel Afonso <Daniel-Af@outlook.com>
2017-12-06 20:14:19 +0000

    Mejorado

--- js/apiMaps.js
+++ js/apiMaps.js
@@ -1,5 +1,6 @@
 var mapProp;
 var map;
+var limites;
 var markers = [];
 var retraso = 1000;
 var retrasoActivo = true;
@@ -8,14 +9,15 @@ var retrasoActivo = true;
  */
 function myMap() {
     // Aplicamos el zoom que corresponda por el dispositivo utilizado
-    var zoom = mediaQ();
+    var ajustes = mediaQ();
     // Propiedades del mapa, lo centrado que está, el zoom y el tipo
     mapProp = {
-        center: new google.maps.LatLng(47.8310667, 15.174334),
-        zoom: zoom,
+        center: new google.maps.LatLng(ajustes[1], ajustes[2]),
+        zoom: ajustes[0],
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
 }
 /**
  * Elimina todos los objetos markers en el mapa.
@@ -33,12 +35,14 @@ function clearOverlays() {
  */
 function marcarBueno(elegidos) {
     clearOverlays();
+    limites = new google.maps.LatLngBounds();
     contador = 0;
     for (var t = 0; t < elegidos.length; t++) {
         setTimeout(function () {
             geolocalizar(elegidos[t]);
         }, retraso * t);
     }
+
 }
 var contador = 0;
 
@@ -74,6 +78,8 @@ function geolocalizar() {
             // Agrega la marca al mapa
             marca.setMap(map);
             markers.push(marca);
+            limites.extend(marca.position);
+            map.fitBounds(limites);
         } else {
             console.log("Existe un problema con las peticiones de localizacion.");
             console.log(status);
@@ -94,125 +100,24 @@ function delay() {
         botonDelay.textContent = "Desactivar"
     }
 }
-/*
-function marcarBueno(elegidos) {
-    clearOverlays();
-
-    elegidos.forEach(element => {
-        var geocoder = new google.maps.Geocoder();
-        var ciclo = element.ciclo;
-        // Geocode devuelve la latitud y la longitud enviandole la ciudad y el pais.
-        geocoder.geocode({
-            address: element.ciudad + ", " + element.pais
-        }, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                // Posicion para colocar la marca.
-                var posicionMarca = new google.maps.LatLng(
-                    results[0].geometry.location.lat(),
-                    results[0].geometry.location.lng()
-                );
-                // Marca que asignaremos al mapa.
-                var marca = new google.maps.Marker({
-                    position: posicionMarca,
-                    animation: google.maps.Animation.BOUNCE,
-                    contentString: ciclo
-                });
-                // Agrega el listener para que al hacer click muestre el mensaje
-                marca.addListener("click",
-                    function () {
-                        // Ventana con la informacion
-                        var infowindow = new google.maps.InfoWindow({
-                            content: ciclo
-                        });
-                        // Asignado al map y marca
-                        infowindow.open(map, marca);
-                    });
-                // Agrega la marca al mapa
-                marca.setMap(map);
-                markers.push(marca);
-            } else {
-                console.log("Existe un problema con las peticiones de localizacion.");
-                console.log(status);
-            }
-        })
-    })
-}
-*/
 
 /**
  * Funcion que modifica el zoom dependiendo del dispositivo
  * que lo está utilizando
  */
 function mediaQ() {
-    let zoom = 4;
-    let query = Modernizr.mq('(min-width: 400px)');
-    if (query) {
-        console.log("Es mayor que ");
-        zoom = 4;
-    } else {
-        console.log("Es menor que ")
-        zoom = 6;
+    var ajustes = [4, 47.8310667, 15.174334];
+    if (Modernizr.mq('all and (max-width: 400px)')) {
+        ajustes[0] = 3;
+        ajustes[1] = 49.869000;
+        ajustes[2] = 16.558000;
+        document.getElementById("googleMap").style = "height:400px"
+    } else if (Modernizr.mq('all and (max-width: 800px)')) {
+        ajustes[0] = 4;
+        document.getElementById("googleMap").style = "height:700px"
+    }else {
+        ajustes[0] = 4;
+        document.getElementById("googleMap").style = "height:800px"
     }
-    return zoom;
+    return ajustes;
 }
-
-/*
-function obtenerPosicion(sitio) {
-    var geocoder = new google.maps.Geocoder();
-    var nuevoSitio;
-    console.log(sitio);
-    geocoder.geocode({
-        'address': sitio.ciudad + ', ' + sitio.pais
-    }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            alert(results[0].geometry.location.lat() + ", " + results[0].geometry.location.lng());
-            
-            nuevoSitio = new movilidadLocalizacion(sitio.tipo, sitio.ciclo, sitio.pais, sitio.ciudad,
-                results[0].geometry.location.lat(), results[0].geometry.location.lng());
-                
-        }
-        console.log(results);
-        console.log(status);
-        console.log(nuevoSitio);
-        elegidos.push(nuevoSitio);
-    });
-}
-function obtenerPosiciones() {
-    var ubicacionElegidos = [];
-    
-    for (let index = 0; index < elegidos.length; index++) {
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode({
-            'address': elegidos[index].ciudad + ', ' + elegidos[index].pais
-        }, function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                alert(results[0].geometry.location.lat() + ", " + results[0].geometry.location.lng());
-                ubicacionElegidos.push(new movilidadLocalizacion(elegidos[index].tipo, elegidos[index].ciclo,
-                    elegidos[index].pais, elegidos[index].ciudad, results[0].geometry.location.lat(), results[0].geometry.location.lng()));
-            }
-            console.log(results);
-            console.log(status);
-        });
-    }
-    console.log(elegidos);
-    console.log(ubicacionElegidos);
-    alert("lo va a enviar");
-    //marcar(ubicacionElegidos);
-}
-function marcar() {
-    var infowindow = new google.maps.InfoWindow();
-    var marker, i;
-    for (i = 0; i < elegidos.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(elegidos[i][4], elegidos[i][5]),
-            map: map
-        });
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-            return function () {
-                infowindow.setContent(elegidos[i][1]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-    }
-}
-*/
