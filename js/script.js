fca1551a9368e6712775f8c480f2056714a9d579
Daniel Afonso <Daniel-Af@outlook.com>
2017-12-06 13:34:16 +0000

    Mejorado?

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ recupearJson();
  * Recupera el Json y lo mete en el array de listaCompleta.
  */
 function recupearJson() {
-    for (let index = 0; index < datos.length; index++) {
-        listaCompleta.push(new movilidad(datos[index].tipo, datos[index].ciclo, datos[index].pais, datos[index].ciudad));
-    }
+    datos.forEach(e => {
+        listaCompleta.push(new movilidad(e.tipo, e.ciclo, e.pais, e.ciudad));
+    })
     crearOpciones();
 }
 /**
@@ -31,13 +31,7 @@ function crearOpciones() {
  * @return aux array con los datos filtrados
  */
 function recuperarTipos(tipo) {
-    var aux = [];
-    /*
-    for (let index = 0; index < listaCompleta.length; index++) {
-        if (listaCompleta[index].tipo.includes(tipo))
-            aux.push(listaCompleta[index]);
-    }
-    */
+    let aux = [];
     listaCompleta.forEach(element => {
         if (element.tipo.includes(tipo)) {
             aux.push(element);
@@ -52,37 +46,30 @@ function recuperarTipos(tipo) {
  */
 function rellenarLimpiando(tipo, filtrados) {
     let contenedor = document.getElementById("opciones");
-    var contenedorCheck = document.getElementById("check");
-    var contenedorCombo = document.getElementById("combo");
+    let contenedorSecundario = document.getElementById("contenedorSecundario");
 
-    if (contenedorCheck != null)
-        deleteTreeElements(contenedorCheck, true);
-    if (contenedorCombo != null)
-        deleteTreeElements(contenedorCombo, true);
+    if (contenedorSecundario != null)
+        deleteTreeElements(contenedorSecundario, true);
+
+    contenedorSecundario = createElement("div", null, "contenedorSecundario", null, null, null, null, null, null, null);
+    let filtradosUnicos = filtrar(tipo, filtrados);
 
     if (tipo == 0) {
-        contenedorCheck = createElement("div", null, "check", null, null, null, null, null, null, null);
-        contenedorCheck.appendChild(createElement("button", "btn btn-outline-info", "btnSeleccionar", null, "Seleccionar/Desseleccionar", "todos()", null, null, null, null));
-
-        let filtradosUnicos = filtrar(tipo, filtrados);
+        contenedorSecundario.appendChild(createElement("button", "btn btn-outline-info", "btnSeleccionar", null, "Seleccionar/Desseleccionar", "todos()", null, null, null, null));
         for (let index = 0; index < filtradosUnicos.length; index++) {
-            contenedorCheck.appendChild(crearCheck(index, filtradosUnicos[index]));
+            contenedorSecundario.appendChild(crearCheck(index, filtradosUnicos[index]));
         }
-        contenedor.appendChild(contenedorCheck);
-        todosMarcados = false;
-        todos();
+        //todosMarcados = false;
+        //todos();
     } else if (tipo == 1) {
-        contenedorCombo = createElement("div", null, "combo", null, null, null, null, null, null, null);
-        let filtradosUnicos = filtrar(tipo, filtrados);
-
         let combo = createElement("select", "custom-select", "comboPaises", null, null, null, null, null, null, null);
         for (let index = 0; index < filtradosUnicos.length; index++) {
             combo.appendChild(createElement("option", null, null, null, filtradosUnicos[index], null, filtradosUnicos[index], null, null, null));
         }
-        contenedorCombo.appendChild(combo);
-        contenedor.appendChild(contenedorCombo);
+        contenedorSecundario.appendChild(combo);
     }
-    buscarMarcados();
+    contenedor.appendChild(contenedorSecundario);
+    //buscarMarcados();
 }
 /**
  * Devuelve un array con los nombres para los check o para el combo,
@@ -92,26 +79,18 @@ function rellenarLimpiando(tipo, filtrados) {
  */
 function filtrar(tipo, arrayFiltrar) {
     var listaAux = [];
+    let tipoMovilidad;
 
-    if (tipo == 0) {
-        for (let index = 0; index < arrayFiltrar.length; index++) {
-            let ciclo = arrayFiltrar[index].ciclo;
-            if (listaAux.indexOf(ciclo) === -1)
-                listaAux.push(ciclo);
-        }
-    } else if (tipo == 1) {
-        for (let index = 0; index < arrayFiltrar.length; index++) {
-            let pais = arrayFiltrar[index].pais;
-            if (listaAux.indexOf(pais) === -1)
-                listaAux.push(pais);
-        }
-    } else if (tipo == 2) {
-        for (let index = 0; index < arrayFiltrar.length; index++) {
-            let ciudad = arrayFiltrar[index].ciudad;
-            if (listaAux.indexOf(ciudad) === -1)
-                listaAux.push(ciudad);
-        }
-    }
+    arrayFiltrar.forEach(e => {
+        if (tipo == 0)
+            tipoMovilidad = e.ciclo;
+        else if (tipo == 1)
+            tipoMovilidad = e.pais;
+        else if (tipo == 2)
+            tipoMovilidad = e.ciudad;
+        if (listaAux.indexOf(tipoMovilidad) === -1)
+            listaAux.push(tipoMovilidad);
+    })
     return listaAux;
 }
 /**
@@ -122,14 +101,14 @@ function buscarMarcados() {
     elegidos = new Array();
     if (tipoRadio == 0) {
         let marcados = document.querySelectorAll("input[name='ciclo']:checked");
-        for (let index = 0; index < marcados.length; index++) {
-            let aux = marcados[index].nextElementSibling.textContent;
+        marcados.forEach(e => {
+            let aux = e.nextElementSibling.textContent;
             filtrados.forEach(element => {
                 if (element.ciclo == aux) {
                     existeLocalizacion(element);
                 }
             });
-        }
+        })
     } else if (tipoRadio == 1) {
         let comboPais = document.getElementById("comboPaises").value;
         filtrados.forEach(element => {
